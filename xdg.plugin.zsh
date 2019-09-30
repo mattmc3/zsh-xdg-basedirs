7920c9e4999c33cffc226463c6a2fafb32f5ee6b
mattmc3 <mattmc3@gmail.com>
2019-09-29 21:25:14 -0400

    Remove anonymous function

--- xdg.plugin.zsh
+++ xdg.plugin.zsh
@@ -4,27 +4,26 @@
 # https://wiki.archlinux.org/index.php/XDG_user_directories
 0=${(%):-%N}
 
-() {
-  # Make sure XDG dirs are set
-  export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
-  export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
-  export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
-  export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-$HOME/.xdg}"
-  USE_XDG_DIRS="${USE_XDG_DIRS:-true}"
 
-  if [[ "$OSTYPE" == darwin* ]]; then
-    export XDG_DESKTOP_DIR="$HOME/Desktop"
-    export XDG_DOCUMENTS_DIR="$HOME/Documents"
-    export XDG_DOWNLOAD_DIR="$HOME/Downloads"
-    export XDG_MUSIC_DIR="$HOME/Music"
-    export XDG_PICTURES_DIR="$HOME/Pictures"
-    export XDG_PUBLICSHARE_DIR="$HOME/Public"
-  fi
+# Make sure XDG dirs are set
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-$HOME/.xdg}"
+USE_XDG_DIRS="${USE_XDG_DIRS:-true}"
 
-  if [[ $USE_XDG_DIRS == true ]]; then
-    local file
-    for file in ${0:A:h}/xdg_support/**/*.xdg.zsh; do
-      source $file
-    done
-  fi
-}
+if [[ "$OSTYPE" == darwin* ]]; then
+  export XDG_DESKTOP_DIR="$HOME/Desktop"
+  export XDG_DOCUMENTS_DIR="$HOME/Documents"
+  export XDG_DOWNLOAD_DIR="$HOME/Downloads"
+  export XDG_MUSIC_DIR="$HOME/Music"
+  export XDG_PICTURES_DIR="$HOME/Pictures"
+  export XDG_PUBLICSHARE_DIR="$HOME/Public"
+fi
+
+if [[ $USE_XDG_DIRS == true ]]; then
+  for file in ${0:A:h}/xdg_support/**/*.xdg.zsh; do
+    source $file
+  done
+  unset file
+fi
